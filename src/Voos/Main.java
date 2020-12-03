5c1cfbbdec6f69993a665192df3918952a81cabb
joao1-barbosa <joao.rbarbosa3@gmail.com>
2020-12-02 23:00:57 -0300

    Muitas Modificações

--- src/Voos/Main.java
+++ src/Voos/Main.java
@@ -1,5 +1,10 @@
 package Voos;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,6 +12,29 @@ public class Main {
 		Voo teste = new Voo("São Paulo","Rio de Janeiro", "30/12/2020");
 		Voo teste2 = new Voo("São Paulo","Rio de Janeiro",700, "30/12/2020", "12:30");
 		
+		Calendar c = Calendar.getInstance();
+		Date data = c.getTime();
+		
+		DateFormat dataf= DateFormat.getDateInstance();
+		DateFormat hora = DateFormat.getTimeInstance();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		String dataFor = sdf.format(data);
+		
+		VooTester ver = new VooTester();
+		
+		if(!ver.verificaAtributos(teste2))
+			System.out.println("Algo deu errado");
+		
+		VooRepository vooRep = new VooRepository();
+		
+		vooRep.pesquisarVoo(teste2);
+		
+		System.out.println(dataFor);
+		System.out.println(dataf.format(data));
+		System.out.println(hora.format(data));
+		System.out.println(c.getTime());
 		System.out.println(teste.hashCode());
 		System.out.println(teste2.hashCode());
 	}
--- src/Voos/VooRepository.java
+++ src/Voos/VooRepository.java
@@ -4,15 +4,46 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class VooRepository {
-	//Mapa do HashCode de origem, destino e data e seu respectivo Voo
+	//Mapa do HashCode de origem, destino e data, e seu respectivo Voo
 	private Map<Integer,Voo> voos;
+
 	
 	//Contrutor do repositório de Voos
 	public VooRepository() {
 		this.voos = new HashMap<Integer,Voo>();
 	}
 	
-	public void cadastrarVoo(Voo objVoo) {
+	//Método que busca o Voo desejado no mapa
+	public Voo pesquisarVoo(Voo objVoo) {
+		Voo buscaVoo = this.voos.get(objVoo.hashCode());
+		VooTester verif = new VooTester();
 		
+		if(!verif.verificaAtributos(objVoo)) {
+			System.out.printf("\nErro ao pesquisar Voo: Dados do Voo inválidos\n");
+		}else {
+			try {
+				buscaVoo.getValorPassagem();
+			} catch (Exception e) {
+				/**Caso não encontre o Voo no mapa, 
+				 * visto que todos seus dados já foram verificados,
+				 * adciona o mesmo como um novo Voo
+				 */
+				adicionarVoo(objVoo);
+			}
+		}
+		return buscaVoo;
+	}
+	
+	
+	//Método que adiciona o novo Voo ao mapa
+	public void adicionarVoo(Voo objVoo) {
+		VooCreator create = new VooCreator();
+		
+		try {
+			this.voos.put(create.criarVoo(objVoo).hashCode(), create.criarVoo(objVoo));
+			System.out.printf("\nNovo Voo cadastrado com sucesso.\n");
+		} catch (Exception e) {
+			System.out.printf("\nErro ao cadastrar novo Voo.\n");
+		}
 	}
 }
